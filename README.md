ef2a5621bf2aca90e4a37c4dfbbb4eb6fa6e24e3
Saurav Dwivedi <saurav.dwivedi@gmail.com>
2023-02-08 22:09:55 +0100

    Add TwoSum.rs

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# Rust
+# A collection of Rust programs I wrote for practice
+
+## Project description
+
+A collection of coding challenge solutions written in Rust. Coding challenge description is given as _doc comments_ on top of each program. I write codes to solve problems from:
+
+- https://www.hackerrank.com/
+- https://code-exercises.com/
+
+## Execution
+
+```mermaid
+gitGraph
+    commit
+    commit
+    branch develop
+    commit
+    commit
+    commit
+    checkout main
+    commit
+    commit
+    merge develop
+    commit
+    commit
+```
+
+### Clone repository  
+
+```bash
+gh repo clone sauravdwivedi/Rust
+```
+
+### Run code
+
+```bash
+cd Rust
+rustc <script_name>.rs
+./<script_name>
+```
+
+### Format code
+
+```bash
+rustfmt <script_name>.rs
+```
+
+### Rust Style guide
+
+- https://doc.rust-lang.org/1.0.0/style/
---
+++ TwoSum.rs
@@ -0,0 +1,47 @@
+/**
+ * Write a method that checks if there is at least one pair of
+ * numbers which sum equals target. arr=[1, 3, 4] and target=5
+ * result is true because the pair (1,4) sums to five.
+ */
+use std::io;
+
+fn two_sum(list_of_numbs: Vec<i32>, target_num: i32) -> String {
+    let mut result: bool = false;
+    let message: String;
+    for p in &list_of_numbs {
+        for q in &list_of_numbs {
+            if p != q && p + q == target_num {
+                result = true;
+            }
+        }
+    }
+    if result == true {
+        message = format!("At least one pair has sum equal to {target_num}!").to_string();
+    } else {
+        message = format!("No pair has sum equal to {target_num}!").to_string();
+    }
+    return message;
+}
+
+fn main() {
+    println! {"Enter list of integers (space separated e.g. '1 2 3'): "};
+    let mut input_line: String = String::new();
+    io::stdin()
+        .read_line(&mut input_line)
+        .expect("Failed to read line");
+    let list_of_numbs: Vec<i32> = input_line
+        .trim()
+        .split(' ')
+        .collect::<Vec<&str>>()
+        .iter()
+        .map(|n| n.parse().expect("Input not an integer"))
+        .collect();
+    println!("Enter target integer: ");
+    let mut input_line: String = String::new();
+    io::stdin()
+        .read_line(&mut input_line)
+        .expect("Failed to read line");
+    let target_num: i32 = input_line.trim().parse().expect("Input not an integer");
+    let result: String = two_sum(list_of_numbs, target_num);
+    println!("{}", result);
+}
